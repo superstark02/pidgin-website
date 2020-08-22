1f92927efb43ef32796cba46866830243a9a1759
superstark02 <superstark02@gmail.com>
2020-08-22 14:41:20 +0530

    8/22/2020

---
+++ client/build/precache-manifest.48185b699d27a05ffe27519f2e7d214a.js
@@ -0,0 +1,206 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed5d27cce90a4b7ece23356436574938",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7757eb0d6467cbb32cd3",
+    "url": "/static/css/2.7b66e74b.chunk.css"
+  },
+  {
+    "revision": "74ac0bce6d5d4fe43b42",
+    "url": "/static/css/main.3cfc286e.chunk.css"
+  },
+  {
+    "revision": "7757eb0d6467cbb32cd3",
+    "url": "/static/js/2.5550bde8.chunk.js"
+  },
+  {
+    "revision": "0db6f044f1139b41123cc0fdc2f8a22f",
+    "url": "/static/js/2.5550bde8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74ac0bce6d5d4fe43b42",
+    "url": "/static/js/main.05777e13.chunk.js"
+  },
+  {
+    "revision": "75e69a9981bd4ef01e4e",
+    "url": "/static/js/runtime-main.6872308a.js"
+  },
+  {
+    "revision": "cedacaa5b0e5eb4969a767c9f79e3a8b",
+    "url": "/static/media/ABeeZee-Italic.cedacaa5.ttf"
+  },
+  {
+    "revision": "55fc1266e3821b3dd3d1a533dcd9c113",
+    "url": "/static/media/ABeeZee-Regular.55fc1266.ttf"
+  },
+  {
+    "revision": "6677d9249501c4c4203aab22ef82f8bf",
+    "url": "/static/media/Friendly-Schoolmates-Regular.6677d924.otf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "4877a37c01b8caabec22fb8b2e847be4",
+    "url": "/static/media/Phone.4877a37c.png"
+  },
+  {
+    "revision": "2260484c32c6c23ff5efdda20a6f6d21",
+    "url": "/static/media/about-wallpaper.2260484c.jpg"
+  },
+  {
+    "revision": "9d24a00020d48655990d615a3f1b2b21",
+    "url": "/static/media/alt-image.9d24a000.jpg"
+  },
+  {
+    "revision": "a6bb1a03dc11e7b96e134e977e78855b",
+    "url": "/static/media/app_bg.a6bb1a03.png"
+  },
+  {
+    "revision": "e2a4464eeeedb9200ec4b03c9a02ee0a",
+    "url": "/static/media/astronaut.e2a4464e.jpg"
+  },
+  {
+    "revision": "56f0ea7d00bc3133e44547b378c8c906",
+    "url": "/static/media/blog-home.56f0ea7d.jpg"
+  },
+  {
+    "revision": "bf0147b2e085da0b5f0a874f266cef90",
+    "url": "/static/media/blog-motivation.bf0147b2.jpg"
+  },
+  {
+    "revision": "22eaed831490e91ce17d9b41938dbdf4",
+    "url": "/static/media/blogs-revision.22eaed83.jpg"
+  },
+  {
+    "revision": "526f3b1184dd906fb283c8ca321f6a03",
+    "url": "/static/media/careers-coder.526f3b11.jpg"
+  },
+  {
+    "revision": "ec65b395319ad36fe3d32dc2db3e524b",
+    "url": "/static/media/careers-content.ec65b395.jpg"
+  },
+  {
+    "revision": "455e266918581b4066c46ba521bc51f0",
+    "url": "/static/media/careers-design.455e2669.jpg"
+  },
+  {
+    "revision": "fbd74bfa9a53d77a52d245689d3b446a",
+    "url": "/static/media/careers-header.fbd74bfa.jpg"
+  },
+  {
+    "revision": "365f3c0f605a6255e210e5dd7c89e378",
+    "url": "/static/media/careers-marketing.365f3c0f.jpg"
+  },
+  {
+    "revision": "facc62b92c96deed255c4d3a8dbd0559",
+    "url": "/static/media/careers-team.facc62b9.jpg"
+  },
+  {
+    "revision": "cba09cedd00375dce1720a45ebee18cb",
+    "url": "/static/media/chemsitry.cba09ced.jfif"
+  },
+  {
+    "revision": "1f461e943b151bb09773a7b8ee28fbbf",
+    "url": "/static/media/classroom.1f461e94.jpg"
+  },
+  {
+    "revision": "6f17ea3f3da88b27d43723e0fa8a9e41",
+    "url": "/static/media/classroom.6f17ea3f.jpg"
+  },
+  {
+    "revision": "b520b5a2410709cbd22b76479e549f64",
+    "url": "/static/media/contact-us-wallpaper.b520b5a2.jpg"
+  },
+  {
+    "revision": "067bbc2ef26f68c808a061d8ba4d077a",
+    "url": "/static/media/contact-us.067bbc2e.jpg"
+  },
+  {
+    "revision": "458006436dc13c210ff4fe058ed37852",
+    "url": "/static/media/doubt-sessions-wallpaper.45800643.jpeg"
+  },
+  {
+    "revision": "4fc395f66650a1495af4a4deb20bf096",
+    "url": "/static/media/find.4fc395f6.jpg"
+  },
+  {
+    "revision": "bba671b89b4ba60c13e02088dd88df62",
+    "url": "/static/media/full-course.bba671b8.jpg"
+  },
+  {
+    "revision": "9a17e3069ef3b78a2d4d6578fb84580a",
+    "url": "/static/media/home-wallpaper.9a17e306.jpg"
+  },
+  {
+    "revision": "d5bb456035bd079c4862c0f3d61dd9b7",
+    "url": "/static/media/home-webinars.d5bb4560.jpg"
+  },
+  {
+    "revision": "482d7d462384fe7aef495734cee4138c",
+    "url": "/static/media/how-to-attempt-jee.482d7d46.jpg"
+  },
+  {
+    "revision": "5c025c96cb3b093dc9905145b90c6afd",
+    "url": "/static/media/iit-b.5c025c96.jpg"
+  },
+  {
+    "revision": "15021f1e120cadd18d160fafc0fff7eb",
+    "url": "/static/media/image4.15021f1e.png"
+  },
+  {
+    "revision": "82024f6980976b2c911fa8c974d35062",
+    "url": "/static/media/individual.82024f69.jpg"
+  },
+  {
+    "revision": "e698ec1a3018c8bbd07c4c166bbc2d3e",
+    "url": "/static/media/jee-faqs.e698ec1a.jpg"
+  },
+  {
+    "revision": "749272868b9a19affedd010fc8edd632",
+    "url": "/static/media/lean-on-us.74927286.jpg"
+  },
+  {
+    "revision": "82419a0eede8294c2a57449d776d63b5",
+    "url": "/static/media/map.82419a0e.jpg"
+  },
+  {
+    "revision": "5600fdb725221f3835cdabc43426dcee",
+    "url": "/static/media/near.5600fdb7.jpg"
+  },
+  {
+    "revision": "1b448703bd3fca661cd7cafd6d6b90c1",
+    "url": "/static/media/no-classes.1b448703.jpg"
+  },
+  {
+    "revision": "01e9945896dba3c323ec6b4ff5a39bf3",
+    "url": "/static/media/online.01e99458.jpg"
+  },
+  {
+    "revision": "bb74de318ca1c722333688f8a55af4ad",
+    "url": "/static/media/profile-bg.bb74de31.jpg"
+  },
+  {
+    "revision": "b13830bab40997a3324060c9a050ed99",
+    "url": "/static/media/steps.b13830ba.png"
+  },
+  {
+    "revision": "db9b21a1c41f3dcd9731e1e7acfdbb57",
+    "url": "/static/media/store.db9b21a1.png"
+  },
+  {
+    "revision": "0189988f60ecdc823611aa442e9caad9",
+    "url": "/static/media/tips.0189988f.jpg"
+  },
+  {
+    "revision": "ef0ba96055787070472da2cbc75cea6c",
+    "url": "/static/media/women.ef0ba960.jpg"
+  },
+  {
+    "revision": "ffce77161f8a9630cb9093ffea9445e2",
+    "url": "/static/media/workshops.ffce7716.jpg"
+  }
+]);
